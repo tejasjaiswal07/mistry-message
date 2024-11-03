b0ecd79c983e0ac7187462e7f5eca99b0bc1fa9f
Tejas Jaiswal <20ec112@jssaten.ac.in>
2024-11-03 08:07:58 +0530

    update: enhance message retrieval with aggregation and error handling

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -18,7 +18,36 @@ export async function GET(request: Request){
             status: 401})
         }
 
+
+        // using this will convert the userID into mongoose objectID 
         const userID = new mongoose.Types.ObjectId(user._id);
+
+        try {
+            
+            const user = await UserModel.aggregate([
+                {$match: {_id: userID}},
+                {$unwind: "$messages"},
+                {$sort: {"messages.createdAt": -1}},
+                {$group: {_id: "$_id", messages: {$push: "$messages"}}}
+            ])
+
+            if(!user || user.length === 0){
+                return Response.json({
+                    success: false,
+                    message: "user not found"
+                },
+            {status: 404})
+            }
+
+            return Response.json({
+                success: true,
+                messages: user[0].messages
+            },
+            {status: 200})
+        } catch (error) {
+            
+        }
+
 
 
 }
